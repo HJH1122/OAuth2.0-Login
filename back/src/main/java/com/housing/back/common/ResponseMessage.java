40ea2105d16ca8043d89c21b23bafcdb027cf881
hjh <ktorn21@naver.com>
2025-08-18 19:27:24 +0900

    id중복 체크 api추가

---
+++ back/src/main/java/com/housing/back/common/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.housing.back.common;
+
+public interface ResponseMessage {
+
+    String SUCCESS = "Sucess.";
+    String VALIDATION_FAIL = "Validation failed.";
+    String DUPLICATE_ID = "Duplicate Id.";
+    String SIGN_IN_FAIL = "Login information mismatch.";
+    String CERTIFICATION_FAIL = "Certification failed.";
+    String DATABASE_ERROR = "Database error.";
+    
+    
+}
---
+++ front/src/types/enums/response-message.enum.ts
@@ -0,0 +1,10 @@
+enum ResponseMessage {
+     SUCCESS = "Sucess.",
+     VALIDATION_FAIL = "Validation failed.",
+     DUPLICATE_ID = "Duplicate Id.",
+     SIGN_IN_FAIL = "Login information mismatch.",
+     CERTIFICATION_FAIL = "Certification failed.",
+     DATABASE_ERROR = "Database error.",
+}
+
+export default ResponseMessage;
